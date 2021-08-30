90962fbb6780cf8915fad661560c7b4d80d8d700
dev-kiplangat <kiplangatsgt@gmail.com>
2021-08-29 21:48:05 -0700

    minor refactor

---
+++ src/components/issue.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="column">
+    <div class="text-h5 text-center q-pa-lg">Issues heighted will be posted here.</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "issue-sect",
+};
+</script>
+
+<style lang="scss" scoped></style>
